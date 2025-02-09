ccbfed5f92c3ae235448a84f8317538ca0c3b303
Bakk Attila <attila.bakk11@gmail.com>
2025-02-09 19:03:44 +0100

    asdxd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
